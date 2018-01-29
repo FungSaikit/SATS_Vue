dc1623a366e05c4471481979f74ee74426a17b50
FungSaikit <675703302@qq.com>
2018-01-29 21:39:22 +0800

    first run commit

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import Header from './components/_header'
+import Footer from './components/_footer'
 
 Vue.config.productionTip = false
+
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
@@ -12,4 +18,14 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
+})
+
+new Vue({
+  el: '#header',
+  render: h => h(Header)
+})
+
+new Vue({
+  el: '#footer',
+  render: h => h(Footer)
 })
