c5ae4fabe172c8a8ad63c9ab1e6a9caf209acf21
FungSaikit <675703302@qq.com>
2018-02-10 00:42:19 +0800

    搜索栏

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
+import HouseList from '@/components/houselist'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/',
+      path: '/index',
       name: '主页',
       component: Index
+    }, 
+    {
+      path: '/house_list/:type', 
+      name: '购房租房', 
+      component: HouseList
     }
   ]
 })
